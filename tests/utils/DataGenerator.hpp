a106c4f7d5720354e8a97772c2adf36a406da406
Rommel Quintanilla <rommel.quintanilla.c@gmail.com>
2021-09-17 01:47:56 -0500

    Adding plot scripts

--- tests/utils/DataGenerator.hpp
+++ tests/utils/DataGenerator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Point.h"
+#include <ostream>
 
 namespace utec {
 namespace spatial {
@@ -26,20 +27,26 @@ struct point_comparator {
 class DataGenerator {
   using data_t = int;
   using point_t = Point<data_t, 2>;
+  using point_set_t = std::set<point_t, point_comparator<data_t, 2>>;
 
  public:
-  DataGenerator() { srand((unsigned)time(0)); }
-
-  using point_set_t = std::set<point_t, point_comparator<data_t, 2>>;
-  point_set_t generate(std::size_t num_points, std::size_t min,
-                       std::size_t max) {
-    point_set_t points;
-
+  DataGenerator(std::size_t num_points, std::size_t min, std::size_t max) {
+    srand((unsigned)time(0));
     for (std::size_t I = 0; I < num_points; I++)
       points.insert(point_t(
           {genRandomNumber<int>(min, max), genRandomNumber<int>(min, max)}));
-    return points;
   }
+
+  point_set_t get_data() { return points; }
+
+  void print(std::ostream& os){
+    for (auto& p : points) {
+      os<<p.get(0)<<" "<<p.get(1)<<"\n";
+    }
+  }
+
+ private:
+  point_set_t points;
 };
 
 }  // namespace spatial
--- tests/validation_test.cpp
+++ tests/validation_test.cpp
@@ -6,6 +6,7 @@
 #include "Point.h"
 #include "Validator.h"
 #include "utils/DataGenerator.hpp"
+#include <fstream>
 
 using namespace utec::spatial;
 
@@ -13,7 +14,6 @@ TEST(SimpleTest, basicTest) {
   using data_t = int;
   using point_t = Point<data_t, 2>;
 
-  DataGenerator generator;
 
   Validator<point_t> validator;
   BasicSpatial<point_t> instancia;
@@ -21,15 +21,28 @@ TEST(SimpleTest, basicTest) {
   const std::size_t num_points = 10000;
   const std::size_t min = 0, max = 1000;
 
-  auto points = generator.generate(num_points, min, max);
+  DataGenerator generator(num_points, min, max);
+  auto points = generator.get_data();
 
   for (auto& p : points) {
     validator.insert(p);
     instancia.insert(p);
   }
 
+  std::ofstream dataset;
+  dataset.open("dataset.txt");
+  generator.print(dataset);
+  dataset.close();
+
   auto reference_result = validator.nearest_neighbor(point_t({50, 50}));
   auto result = instancia.nearest_neighbor(point_t({50, 50}));
+
+  std::ofstream output;
+  output.open("output.txt");
+  output<<50<<" "<<50<<" "<<0<<"\n";
+  output<<reference_result.get(0)<<" "<<reference_result.get(1)<<" 1"<<"\n";
+  output<<result.get(0)<<" "<<result.get(1)<<" 2"<<"\n";
+  output.close();
 
   EXPECT_EQ(reference_result, result);
 }
